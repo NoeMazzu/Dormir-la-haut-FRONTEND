7daf1ecc22c27c8358754bf02f1b7845b36dbe20
Noé <n.mazzu@hotmail.fr>
2024-03-07 12:07:33 +0100

    commit peter

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -6,54 +6,85 @@ import {
   TouchableOpacity,
 } from "react-native";
 import MapView, { Marker } from "react-native-maps";
-import * as React from 'react';
+import * as React from "react";
 import { ImageSlider } from "react-native-image-slider-banner";
 
 export default function HomeScreen({ navigation }) {
-
-  const gallery =  [
-      {img:"https://source.unsplash.com/1024x768/?nature"},
-      {img:"https://source.unsplash.com/1024x768/?water"},
-      {img:"https://source.unsplash.com/1024x768/?girl"},
-      {img:"https://source.unsplash.com/1024x768/?tree"}];
-
+  const gallery = [
+    { img: "https://source.unsplash.com/1024x768/?nature" },
+    { img: "https://source.unsplash.com/1024x768/?water" },
+    {
+      img: "https://media.licdn.com/dms/image/D4D03AQFJjZ-tYqWp9A/profile-displayphoto-shrink_200_200/0/1676368422779?e=2147483647&v=beta&t=5vOpwfuZMga8eaL7P_riZ9_hsuIBvN6A4P70IhsWx-k",
+    },
+    { img: "https://source.unsplash.com/1024x768/?tree" },
+  ];
 
   return (
     <View style={styles.container}>
       <View style={styles.topContainer}>
-        <View style={styles.meteoContainer} >
-          <TouchableOpacity style={styles.meteoButton} onPress={()=> {navigation.navigate('MeteoScreen')}}>
+        <View style={styles.meteoContainer}>
+          <TouchableOpacity
+            style={styles.meteoButton}
+            onPress={() => {
+              navigation.navigate("MeteoScreen");
+            }}
+          >
             <Text style={styles.textTitle}>METEOContainer</Text>
           </TouchableOpacity>
         </View>
         <View style={styles.highRigtContainers}>
           <View style={styles.actusContainers}>
-            <TouchableOpacity style={styles.buttonNews} onPress={()=> {navigation.navigate('NewsScreen')}}>
+            <TouchableOpacity
+              style={styles.buttonNews}
+              onPress={() => {
+                navigation.navigate("NewsScreen");
+              }}
+            >
               <Text>Voir les news</Text>
             </TouchableOpacity>
           </View>
           <View style={styles.cheklistContainers}>
-            <TouchableOpacity  style={styles.checklistButton} onPress={()=> {navigation.navigate('ChecklistsScreen')}}>
-            <Text>CHECKLISTS / Small Containers</Text>
-          </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.checklistButton}
+              onPress={() => {
+                navigation.navigate("ChecklistsScreen");
+              }}
+            >
+              <Text>CHECKLISTS / Small Containers</Text>
+            </TouchableOpacity>
           </View>
-          
         </View>
       </View>
-      <TouchableOpacity style={styles.mapContainer} onPress={()=> {navigation.navigate('MapScreen')}}>
+      <TouchableOpacity
+        style={styles.mapContainer}
+        onPress={() => {
+          navigation.navigate("MapScreen");
+        }}
+      >
         <MapView
-        mapType="terrain"
-        initialRegion={{
-          latitude: 45.7,
-          longitude: 6.4,
-          latitudeDelta: 2,
-          longitudeDelta: 2,
+          mapType="terrain"
+          initialRegion={{
+            latitude: 45.7,
+            longitude: 6.4,
+            latitudeDelta: 2,
+            longitudeDelta: 2,
           }}
-          style={{ flex: 1, height: '100%', width: '100%', borderRadius: 10, }}
-          sharedTransitionTag="tag"></MapView>
+          style={{ flex: 1, height: "100%", width: "100%", borderRadius: 10 }}
+          sharedTransitionTag="tag"
+        ></MapView>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.photoContainer} onPress={()=> {navigation.navigate('PhotosScreen')}}>
-        <ImageSlider data={gallery} autoPlay preview={false} caroselImageStyle={{height: '100%'}}/>
+      <TouchableOpacity
+        style={styles.photoContainer}
+        onPress={() => {
+          navigation.navigate("PhotosScreen");
+        }}
+      >
+        <ImageSlider
+          data={gallery}
+          autoPlay
+          preview={false}
+          caroselImageStyle={{ height: "100%" }}
+        />
       </TouchableOpacity>
     </View>
   );
@@ -72,13 +103,13 @@ const styles = StyleSheet.create({
     width: "50%",
     padding: 4,
   },
-  meteoButton:{
-    flex:1,
+  meteoButton: {
+    flex: 1,
     borderRadius: 10,
-    backgroundColor: '#35357F'
+    backgroundColor: "#35357F",
   },
-  textTitle:{
-    color: 'white',
+  textTitle: {
+    color: "white",
   },
   highRigtContainers: {
     width: "50%",
@@ -88,20 +119,20 @@ const styles = StyleSheet.create({
     height: "50%",
     padding: 4,
   },
-  buttonNews : {
-    flex:1,
+  buttonNews: {
+    flex: 1,
     borderRadius: 10,
-    backgroundColor: '#35357F'
+    backgroundColor: "#35357F",
   },
   cheklistContainers: {
     backgroundColor: "lightgreen",
     height: "50%",
     padding: 4,
   },
-  checklistButton:{
-    flex:1,
+  checklistButton: {
+    flex: 1,
     borderRadius: 10,
-    backgroundColor: '#35357F'
+    backgroundColor: "#35357F",
   },
   topContainer: {
     flexDirection: "row",
@@ -112,7 +143,7 @@ const styles = StyleSheet.create({
     backgroundColor: "purple",
     width: "100%",
     height: "30%",
-    padding:4,
+    padding: 4,
   },
   photoContainer: {
     backgroundColor: "pink",
