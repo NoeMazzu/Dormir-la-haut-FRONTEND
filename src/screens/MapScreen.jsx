602a70b8a97efc1783f7d1303acd208c1a29f8e6
estebang-bang <estebanballet@gmail.com>
2024-03-11 10:01:07 +0100

    Préparation affichage modal

--- src/screens/MapScreen.jsx
+++ src/screens/MapScreen.jsx
@@ -3,12 +3,13 @@ import MapView, { Marker } from "react-native-maps";
 import { Dimensions } from "react-native";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { ModalMapScreen } from "../components/ModalMapScreen"
+import { ModalMapScreen } from "../components/ModalMapScreen";
 
 export default function MapScreen({ navigation }) {
   const POIs = useSelector(({ poi }) => poi.value.POIs);
   const user = useSelector((token) => token.user.value.token);
 
+  const [isModalVisible, setIsModalVisible] = useState(false);
   const [markers, setMarkers] = useState([]);
   // Je récupère les infos des pois depuis le store redux et j'en garde seulement 50 sur la mapScreen.
   // Puis je l'enregistre dans un état local.
@@ -16,10 +17,13 @@ export default function MapScreen({ navigation }) {
     if (POIs.length > 0) {
       // j'ai ajouté un if => vérifier si il y a les pois dans le store avant de les push
       setMarkers(POIs.slice(0, 50));
+      console.log(JSON.stringify(markers.length, null, 2));
     }
   }, []);
 
-  // console.log(JSON.stringify(markers.length, null, 2))
+  const showModal = () => {
+    setIsModalVisible(!isModalVisible);
+  };
 
   const renderMarkers = () => {
     return markers.map((poi, i) => {
@@ -56,7 +60,7 @@ export default function MapScreen({ navigation }) {
         pinColor="#fecb2d"
         coordinate={{ latitude: 45.7542305, longitude: 4.8386187 }}
       />
-      {renderMarkers()}
+      {showModal()}
     </MapView>
   );
 }
