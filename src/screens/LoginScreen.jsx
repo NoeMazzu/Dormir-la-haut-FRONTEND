ab528954bdbd23c523c25c35c93702ebe3e4bc9b
PeterHdz7 <hernandezpeter38230@gmail.com>
2024-03-11 12:26:37 +0100

    Text input Email Password Login Screen change

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,20 +1,28 @@
 import React from "react";
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, ImageBackground, KeyboardAvoidingView, Platform, Dimensions} from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  ImageBackground,
+  KeyboardAvoidingView,
+  Platform,
+  Dimensions,
+} from "react-native";
 import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { setToken } from "../redux/slices/user";
 import { useSelector } from "react-redux";
 
-
 const LoginScreen = ({ navigation }) => {
-  
-const user = useSelector((state) => state.user.value);
+  const user = useSelector((state) => state.user.value);
 
-useEffect(() => {
-  if (user?.token) {
-    navigation.navigate("TabNavigator");
-  }
-}, []);
+  useEffect(() => {
+    if (user?.token) {
+      navigation.navigate("TabNavigator");
+    }
+  }, []);
 
   const [imageLoaded, setImageLoaded] = useState(false);
   const [error, setError] = useState("");
@@ -22,78 +30,90 @@ useEffect(() => {
   const handleImageLoad = () => {
     setImageLoaded(true);
   };
-  const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{9,}$/;
+  const passwordRegex =
+    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{9,}$/;
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   const handleConnection = () => {
-  if (!emailRegex.test(email)) {
-    setError("Adresse e-mail ou mot de passe invalide.");
-    return;
-  }else if (!passwordRegex.test(password)) {
+    if (!emailRegex.test(email)) {
+      setError("Adresse e-mail ou mot de passe invalide.");
+      return;
+    } else if (!passwordRegex.test(password)) {
       setError("Adresse e-mail ou mot de passe invalide.");
       return;
     }
 
-  
-      fetch("https://dormir-la-haut-backend.vercel.app/users/signin", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          mail: email,
-          password: password,
-        }),
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          if (data.result) {
-            setEmail("");
-            setPassword('');
-            setError("");
-            dispatch(setToken(data.token));
-            navigation.navigate('TabNavigator')
-          }else {       
-            setError(data.error);
-          }
-        });
-    };
+    fetch("https://dormir-la-haut-backend.vercel.app/users/signin", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        mail: email,
+        password: password,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.result) {
+          setEmail("");
+          setPassword("");
+          setError("");
+          dispatch(setToken(data.token));
+          navigation.navigate("TabNavigator");
+        } else {
+          setError(data.error);
+        }
+      });
+  };
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === "ios" ? "padding" : "height"}
       style={{ flex: 1, height: Dimensions.get("window").height }}
     >
-    <ImageBackground
-      source={require("../assets/img/Image-background.jpg")}
-      resizeMode="cover"
-      style={styles.background}
-      onLoad={handleImageLoad}
-    >
-            {imageLoaded ? (
-      <View style={styles.filter}>
-        {error ? <Text style={styles.errorText}>{error}</Text> : null}
-        <Text style={styles.title}>Se connecter</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="E-mail"
-          placeholderTextColor="#808080"
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Password"
-          placeholderTextColor="#808080"
-          secureTextEntry
-        />
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.cancelButton} onPress={() => navigation.navigate('LoadingScreen')}>
-            <Text style={styles.buttonText}>Annuler</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.signupButton} onPress={handleConnection}>
-            <Text style={styles.buttonText2} >Se Connecter</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-      ) : null}
-    </ImageBackground>
+      <ImageBackground
+        source={require("../assets/img/Image-background.jpg")}
+        resizeMode="cover"
+        style={styles.background}
+        onLoad={handleImageLoad}
+      >
+        {imageLoaded ? (
+          <View style={styles.filter}>
+            {error ? <Text style={styles.errorText}>{error}</Text> : null}
+            <Text style={styles.title}>Se connecter</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="E-mail"
+              placeholderTextColor="#808080"
+              value={email}
+              onChangeText={(text) => setEmail(text)}
+            />
+
+            <TextInput
+              style={styles.input}
+              placeholder="Password"
+              placeholderTextColor="#808080"
+              secureTextEntry={true} // or secureTextEntry
+              value={password}
+              onChangeText={(text) => setPassword(text)}
+            />
+
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity
+                style={styles.cancelButton}
+                onPress={() => navigation.navigate("LoadingScreen")}
+              >
+                <Text style={styles.buttonText}>Annuler</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.signupButton}
+                onPress={handleConnection}
+              >
+                <Text style={styles.buttonText2}>Se Connecter</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        ) : null}
+      </ImageBackground>
     </KeyboardAvoidingView>
   );
 };
@@ -159,9 +179,8 @@ const styles = StyleSheet.create({
     fontSize: 16,
   },
   title: {
-    color: '#ffffff',
+    color: "#ffffff",
     fontSize: 60,
-    
   },
 });
 
