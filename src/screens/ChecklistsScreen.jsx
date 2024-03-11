b6642a5a1b64fa869ddd9b2bd191ea2a649bcd0e
Charles Bréton <charlesbreton@outlook.com>
2024-03-11 18:01:04 +0100

    pushCharlesV2

--- src/screens/ChecklistsScreen.jsx
+++ src/screens/ChecklistsScreen.jsx
@@ -1,12 +1,8 @@
 import { StyleSheet, Text, View, FlatList, ScrollView } from "react-native";
 import { useSelector } from "react-redux";
-<<<<<<< HEAD
 import ChecklistsEditingForm from "../components/Checklists/ChecklistsEditingForm";
 import ChecklistsForm from "../components/Checklists/ChecklistsForm";
 import { useState } from "react";
-=======
-import { useEffect } from "react";
->>>>>>> b7f7be0d060e702440e2aa66db181efd46d8282f
 
 export default function ChecklistsScreen ({navigation}) {
   const user = useSelector((state) => state.user.value);
