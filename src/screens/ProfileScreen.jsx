4ecbad6dd3ea67d6c65b2195a304e5dfacc4845e
jfbonglet <jean.francois.bonglet@gmail.com>
2024-03-11 10:54:07 +0100

    MAJ profile screen - intègre présentation par Tab

--- src/screens/ProfileScreen.jsx
+++ src/screens/ProfileScreen.jsx
@@ -1,11 +1,17 @@
 import { StyleSheet, Text, View } from "react-native";
 import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
+import { Tab, TabView } from '@rneui/themed';
+import React from 'react';
 
 export default function ProfileScreen({ navigation }) {
+
   const user = useSelector((state) => state.user.value);
   const [poisFav, setPoisFav] = useState([]);
-  console.log('[USER TOKEN:',user.token)
+  const [index, setIndex] = React.useState(0); //Utilisé pour la gestion du TAB
+
+  console.log('[USER TOKEN:',user.token) //TODO A supprimer
+  console.log('[USER:',user) //TODO A supprimer
 
   if (user?.token) {
     navigation.navigate("TabNavigator");
@@ -14,7 +20,7 @@ export default function ProfileScreen({ navigation }) {
   useEffect(() => {
     const myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
-    myHeaders.append("Authorization", `Bearer 8Yy2ldbvcyKsOGd3qxhM02Vu7QOqfgfq`);
+    myHeaders.append("Authorization", `Bearer 8Yy2ldbvcyKsOGd3qxhM02Vu7QOqfgfq`); //${user.token}
 
     // const urlencoded = new URLSearchParams();
     // urlencoded.append("token");
@@ -43,18 +49,47 @@ export default function ProfileScreen({ navigation }) {
     },[]);
 
   console.log(["POIS"], poisFav);
+
   return (
     <View style={styles.container}>
-      <Text>ProfilScreen</Text>
+      <Text>{user.username}</Text>
+      <Tab
+        value={index}
+        onChange={(e) => setIndex(e)}
+        indicatorStyle={{
+          backgroundColor: 'white',
+          height: 3,
+        }}
+        // variant="primary"
+      >
+        <Tab.Item
+          title="Mes favoris"
+          titleStyle={{ fontSize: 12, color:'white', }}
+          icon={{ name: 'heart-o', type: 'font-awesome', color: 'white' }}
+        />
+        <Tab.Item
+          title="Mes checklists"
+          titleStyle={{ fontSize: 12, color:'white' }}
+          icon={{ name: 'check-square-o', type: 'font-awesome', color: 'white' }}
+        />
+      </Tab>
+
+      <TabView value={index} onChange={setIndex} animationType="spring">
+        <TabView.Item style={{ backgroundColor: 'red', width: '100%' }}>
+          <Text h1>Contenu Mes favoris</Text>
+        </TabView.Item>
+        <TabView.Item style={{ backgroundColor: 'green', width: '100%' }}>
+          <Text h1>Contenu Mes checklists</Text>
+        </TabView.Item>
+      </TabView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+container: {
+    backgroundColor: "#161D46",
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+    width: "100%",
+    paddingTop: 60,
+},});
