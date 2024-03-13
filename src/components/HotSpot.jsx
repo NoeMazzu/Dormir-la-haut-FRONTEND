b796c2c4d6668f29c864d4f6a75f659716ce3ef8
estebang-bang <estebanballet@gmail.com>
2024-03-13 10:13:32 +0100

    map google pour tout le monde !

--- src/components/HotSpot.jsx
+++ src/components/HotSpot.jsx
@@ -9,7 +9,7 @@ function HotSpot(props) {
   const token = useSelector((state) => state.user.value.token);
   const poiFound = pois.find((element) => element.name === props.name);
 
-  console.log("[HOTSPOT POI.ID]", poiFound._id);
+  // console.log("[HOTSPOT POI.ID]", poiFound._id);
 
   const handleBookmark = () => {
     return fetch("https://dormir-la-haut-backend.vercel.app/poi/poiBookMark", {
@@ -23,7 +23,7 @@ function HotSpot(props) {
       }),
     })
       .then((response) => response.json())
-      .then((data) => console.log(data))
+      // .then((data) => console.log(data))
       .catch((error) => console.error("Erreur :", error));
   };
 
